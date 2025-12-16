e0d3a71b8810bf2345c620cee937c796dc5764cf
i-m-kartik-01 <guptakartik2146@gmail.com>
2025-12-16 23:05:44 +0530

    fix: updated server/index for env based configuration

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
+require("dotenv").config();
 const http = require("http");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const { Server } = require("socket.io");
-
+const FRONTEND_URL = process.env.FRONTEND_URL;
 const connectDB = require("./connection");
 const userRouter = require("./routes/userRoutes");
 const canvasRouter = require("./routes/canvasRoutes");
 
-require("dotenv").config();
 
 const app = express();
 const PORT = 5003;
@@ -18,7 +18,7 @@ const PORT = 5003;
    ========================= */
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: FRONTEND_URL,
     credentials: true,
   })
 );
@@ -38,7 +38,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: FRONTEND_URL,
     methods: ["GET", "POST"],
     credentials: true,
   },
