e73e858a19f72d20987acfe46d51bd6a36326ec1
i-m-kartik-01 <guptakartik2146@gmail.com>
2025-12-17 12:48:15 +0530

    updated socket.js

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,11 +1,17 @@
 import { io } from "socket.io-client";
 
 const socket = io(process.env.REACT_APP_API_URL, {
+  path: "/socket.io",
   withCredentials: true,
+  transports: ["websocket", "polling"],
 });
 
 socket.on("connect", () => {
   console.log("Connected to WebSocket:", socket.id);
 });
 
-export default socket; // ⭐ THIS LINE WAS MISSING
+socket.on("connect_error", (err) => {
+  console.error("Socket error:", err.message);
+});
+
+export default socket;
